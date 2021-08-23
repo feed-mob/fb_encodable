e1653852ef0198817000172e53fc0fa281b4f76d
steven <wuqi0509@gmail.com>
2021-08-23 17:10:32 +0800

    Add url encode feature

--- lib/encodable.rb
+++ lib/encodable.rb
@@ -4,5 +4,30 @@ require_relative "encodable/version"
 
 module Encodable
   class Error < StandardError; end
-  # Your code goes here...
+
+  class uri
+    attr_reader :domain, :original_query
+
+    def initialize(url)
+      @domain, @original_query = url.split('?', 2)
+    end
+
+    def call
+      return domain || '' if original_query.blank?
+
+      "#{domain}?#{encode_params}"
+    end
+
+    def self.call(url)
+      new(url).call
+    end
+
+    private
+
+    def encode_params
+      URI.encode_www_form(
+        original_query.split('&')&.map { |q| q.split('=', 2) }
+      )
+    end
+  end
 end
